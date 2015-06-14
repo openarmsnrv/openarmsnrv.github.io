cd5e1817f7f7661cf75b930d931948215678601f
kevPo <kevposton@gmail.com>
2015-06-14 08:43:52 -0400

    more tweaks

--- javascript/index.js
+++ javascript/index.js
@@ -1,36 +1,41 @@
-function makeFullscreen() {
-		$('section.fullscreen').each(function(){
-			win_height = $(window).height();
-			navbar_height = $('body > .navbar').height();
-			available_height = win_height - navbar_height;
-			$(this).css('min-height', available_height + 'px');
-		});
-	}
+$(document).ready(function() {
+  function makeFullscreen() {
+      $('section.fullscreen').each(function(){
+        win_height = $(window).height();
+        navbar_height = $('body > .navbar').height();
+        available_height = win_height - navbar_height;
+        $(this).css('height', win_height + 'px');
+        console.log(win_height, navbar_height, available_height);
+      });
+    }
 
-	makeFullscreen();
+    makeFullscreen();
 
-	$(window).resize(function(){
-		makeFullscreen();
-	});
+    $(window).resize(function(){
+      makeFullscreen();
+    });
 
-	if( $().superslides ) {
-		$('.superslides').each(function(){
+    if( $().superslides ) {
+      $('.superslides').each(function(){
 
-			play            = $(this).attr('data-pause');
-			animation_speed = $(this).attr('data-animation');
-			animation       = $(this).attr('data-effect');
-			pagination      = $(this).attr('data-pagination');
-			width           = $(this).attr('data-width');
-			height          = $(this).attr('data-height');
+        play            = $(this).attr('data-pause');
+        animation_speed = $(this).attr('data-animation');
+        animation       = $(this).attr('data-effect');
+        pagination      = $(this).attr('data-pagination');
+        width           = $(this).attr('data-width');
+        height          = $(this).attr('data-height');
 
-			$('.superslides').superslides({
-				play: play,
-				animation_speed: animation_speed,
-				animation: animation,
-				pagination: false,
-				inherit_height_from: width,
-				inherit_width_from: height
-			});
+        $('.superslides').superslides({
+          play: play,
+          animation_speed: animation_speed,
+          animation: animation,
+          pagination: false,
+          inherit_height_from: width,
+          inherit_width_from: height
+        });
 
-		});
-	}
+        console.log('width = ' + $(width).width(), 'height = ' + $(height).height());
+
+      });
+    }
+});
