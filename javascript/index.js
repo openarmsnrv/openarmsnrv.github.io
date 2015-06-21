56ee141d82e726cf928177ed9e3e8b3a919dbba5
Kevin Poston <kevposton@gmail.com>
2015-06-20 22:47:28 -0400

    Added a section, fixed a ton of stuff

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+  $('body').scrollspy({target:'.navbar-collapse', offset:10});
+  
   function makeFullscreen() {
       $('section.fullscreen').each(function(){
-        win_height = $(window).height();
-        navbar_height = $('body > .navbar').height();
-        available_height = win_height - navbar_height;
-        $(this).css('height', win_height + 'px');
+        var win_height = $(window).height();
+        var navbar_height = $('body > .navbar').height();
+        var available_height = win_height;
+        $(this).css('min-height', available_height + 'px');
         console.log(win_height, navbar_height, available_height);
       });
     }
@@ -17,25 +19,55 @@ $(document).ready(function() {
 
     if( $().superslides ) {
       $('.superslides').each(function(){
-
-        play            = $(this).attr('data-pause');
-        animation_speed = $(this).attr('data-animation');
-        animation       = $(this).attr('data-effect');
-        pagination      = $(this).attr('data-pagination');
-        width           = $(this).attr('data-width');
-        height          = $(this).attr('data-height');
-
+        
         $('.superslides').superslides({
-          play: play,
-          animation_speed: animation_speed,
-          animation: animation,
+          play: 5000,
+          animation_speed: "slow",
+          animation: "fade",
           pagination: false,
-          inherit_height_from: width,
-          inherit_width_from: height
+          inherit_height_from: "#start",
+          inherit_width_from: "#start"
         });
 
-        console.log('width = ' + $(width).width(), 'height = ' + $(height).height());
+        console.log('width = ' + $('#start').width(), 'height = ' + $('#start').height());
 
       });
     }
 });
+
+//function makeFullscreen() {
+//		$('section.fullscreen').each(function(){
+//			win_height = $(window).height();
+//			navbar_height = $('body > .navbar').height();
+//			available_height = win_height - navbar_height;
+//			$(this).css('min-height', available_height + 'px'); 
+//		});
+//	}
+//
+//	makeFullscreen();
+//
+//	$(window).resize(function(){
+//		makeFullscreen();
+//	});
+//
+//	if( $().superslides ) {
+//		$('.superslides').each(function(){
+//
+//			play            = $(this).attr('data-pause');
+//			animation_speed = $(this).attr('data-animation');
+//			animation       = $(this).attr('data-effect');
+//			pagination      = $(this).attr('data-pagination');
+//			width           = $(this).attr('data-width');
+//			height          = $(this).attr('data-height');
+//
+//			$('.superslides').superslides({
+//				play: play,
+//				animation_speed: animation_speed,
+//				animation: animation,
+//				pagination: false,
+//				inherit_height_from: width,
+//				inherit_width_from: height
+//			});
+//
+//		});
+//	}
