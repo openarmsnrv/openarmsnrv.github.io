3ca310248edaa832daa593e91b4960519e291913
Kevin Poston <kevposton@gmail.com>
2015-08-28 23:31:38 -0400

    making progress

--- javascript/index.js
+++ javascript/index.js
@@ -60,6 +60,18 @@ $(document).ready(function() {
     }
 
     $("#staff-carousel").owlCarousel({items : 4});
+    $("#testimonial-carousel").owlCarousel(
+      {
+        singleItem: true,
+        autoPlay: true,
+        stopOnHover: true,
+        navigation: true,
+        pagination: false,
+        navigationText: [
+					"<i class='glyphicon glyphicon-menu-left testimonial-nav' aria-hidden='true'></i>",
+					"<i class='glyphicon glyphicon-menu-right testimonial-nav' aria-hidden='true'></i>"
+				]
+      });
 
 });
 
